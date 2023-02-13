da26aca17a9a19ee9239386dea880617490ef666
opilane <opilane@HP-CZC0359NN7>
2023-02-13 13:01:34 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Foreach1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nimed = { "Antont Tammsaare", "Lydia Koidula", "August Gailit", "Betti Alver" };
+
+            foreach (string i in nimed)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
